b7372138f05082d064eec62ee59f294cb2164ecb
Nichlas Torgersen <fristurt@hotmail.com>
2015-03-24 17:17:54 +0100

    php api start

--- php/DBApi.php
+++ php/DBApi.php
@@ -3,16 +3,24 @@ require_once("Question.php");
 
 class DBApi {
 
-    public function __construct() {
+    private $connection,
+            $config;
 
+    public function __construct(array $config) {
+        $this->connection = new PDO(
+                                "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}",
+                                $config['username'],
+                                $config['password']
+                            );
     }
 
-    public function getQuestions() {
+    public function getQuestions($type) {
+        $questions = array();
+
         $q1 = new Question(1, "What is life purpose?", "42", ["42", "43"]);
         $q2 = new Question(2, "What is yellow?", "yellow", ["yellow", "green"]);
         $q3 = new Question(3, "Can has?", "yes", ["yes", "no", "maybe", "not"]);
 
-        $questions = array();
         array_push($questions, $q1);
         array_push($questions, $q2);
         array_push($questions, $q3);
---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+    function config () {
+        return array(
+            "host" => "localhost",
+            "port" => 3306,
+            "dbname" => "int1000",
+            "username" => "root",
+            "password" => ""
+        );
+    }
