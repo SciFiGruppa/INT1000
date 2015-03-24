c95d5771847967b07d9121da23ba420ff56935c7
Nichiatu <fristurt@hotmail.com>
2015-03-24 22:12:50 +0100

    getquestions working for quiz

--- php/DBApi.php
+++ php/DBApi.php
@@ -14,18 +14,62 @@ class DBApi {
                             );
     }
 
+    /**
+     * returns questions based on type of question
+     *
+     * @param string $type Enum declared in database to sort between 'quiz' or 'dropdown'
+     *
+     * @return array Returns an array of objects
+     */
     public function getQuestions($type) {
+        if (!$type) {
+            return;
+        }
+
         $questions = array();
 
-        $q1 = new Question(1, "What is life purpose?", "42", ["42", "43"]);
-        $q2 = new Question(2, "What is yellow?", "yellow", ["yellow", "green"]);
-        $q3 = new Question(3, "Can has?", "yes", ["yes", "no", "maybe", "not"]);
+        $sql = "SELECT answerquestion.questionID, question.correctAnswerID, question.questionText, answerquestion.answerID, answer.answerText FROM answerquestion
+                INNER JOIN question ON (
+                    question.questionID = answerquestion.questionID AND
+                    question.quizType = :type
+                )
+                INNER JOIN answer ON (
+                    answer.answerID = answerquestion.answerID
+                )";
 
-        array_push($questions, $q1);
-        array_push($questions, $q2);
-        array_push($questions, $q3);
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(':type', $type);
+        $stmt->execute();
+
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($this->loopThrough($res) as $qid => $values) {
+            $questions[] = new Question($qid, $values['questionText'], $values['correctAnswerID'], $values['answerText']);
+        }
+
+        echo '<pre>';
+        var_dump($questions);
+        echo '</pre>';
 
         return $questions;
+    }
+    /**
+     * cleans an array so it can be instantiated
+     * 
+     * @param array $result The array returned from the database
+     * 
+     * @return array Returns the cleaned array so it can easily be used to instantiate the Question class
+     */
+    private function loopThrough (array $result) {
+        $output = array();
+
+        foreach ($result as $key => $value) {
+            $output[$value['questionID']]['questionText'] = $value['questionText'];
+            $output[$value['questionID']]['correctAnswerID'] = $value['correctAnswerID'];
+            $output[$value['questionID']]['answerText'][$value['answerID']] = $value['answerText'];
+        }
+
+        return $output;
     }
 
 }
