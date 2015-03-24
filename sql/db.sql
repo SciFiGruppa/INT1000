8f69669bcf475b0b8ac63cc4baa3e43fdf45b012
Vecc <nei>
2015-03-24 16:00:53 +0100

    la til sql

--- sql/db.sql
+++ sql/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 24, 2015 at 03:46 PM
+-- Generation Time: Mar 24, 2015 at 04:00 PM
 -- Server version: 5.5.36
 -- PHP Version: 5.5.8
 
@@ -50,38 +50,6 @@ CREATE TABLE IF NOT EXISTS `answerquestion` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bilde`
---
-
-CREATE TABLE IF NOT EXISTS `bilde` (
-  `bildenr` int(50) NOT NULL AUTO_INCREMENT,
-  `opplastingsdato` date NOT NULL COMMENT 'Dato for opplastning',
-  `filnavn` varchar(50) NOT NULL,
-  `beskrivelse` varchar(400) NOT NULL,
-  PRIMARY KEY (`bildenr`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=63 ;
-
---
--- Dumping data for table `bilde`
---
-
-INSERT INTO `bilde` (`bildenr`, `opplastingsdato`, `filnavn`, `beskrivelse`) VALUES
-(47, '2015-03-02', '10624154f4549abb1e6.gif', 'GIF'),
-(50, '2015-03-02', '10624154f454d33bd2e.png', 'PNG'),
-(51, '2015-03-02', '10624154f45c76b0985.gif', 'gif-bilde'),
-(52, '2015-03-02', '10624154f45cad79bd5.jpeg', 'jpg-bilde'),
-(53, '2015-03-02', '10624154f45cb7d83f2.png', 'png-bilde'),
-(54, '2015-03-02', '10624154f4980ef1b56.jpeg', 'dasdas'),
-(55, '2015-03-02', '10624154f4cc762dbee.gif', 'kljjkkj'),
-(56, '2015-03-03', '10624154f59be9d48c7.jpeg', 'tetetetete'),
-(59, '2015-03-03', '10624154f5a18ec1534.jpeg', 'jjj'),
-(60, '2015-03-03', '10624154f5aa5084b97.gif', 'geir-gif'),
-(61, '2015-03-03', '10624154f5aa6862a9d.jpeg', 'geir-jpg'),
-(62, '2015-03-03', '10624154f5aa7cc6f58.png', 'geir-png');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `flagg`
 --
 
@@ -95,27 +63,6 @@ CREATE TABLE IF NOT EXISTS `flagg` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `klasse`
---
-
-CREATE TABLE IF NOT EXISTS `klasse` (
-  `klassekode` char(30) NOT NULL,
-  `klassenavn` char(50) NOT NULL,
-  PRIMARY KEY (`klassekode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `klasse`
---
-
-INSERT INTO `klasse` (`klassekode`, `klassenavn`) VALUES
-('IS1', 'IS1-Klassen'),
-('IS2', 'IS2-Klassen'),
-('IS3', 'IS3-Klassen');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `question`
 --
 
@@ -123,32 +70,10 @@ CREATE TABLE IF NOT EXISTS `question` (
   `questionID` int(11) NOT NULL AUTO_INCREMENT,
   `questionText` varchar(300) DEFAULT NULL,
   `correctAnswerID` int(11) NOT NULL,
+  `quizType` enum('quiz','dropdown') NOT NULL,
   PRIMARY KEY (`questionID`),
   KEY `correctAnswerID` (`correctAnswerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student`
---
-
-CREATE TABLE IF NOT EXISTS `student` (
-  `brukernavn` char(50) NOT NULL,
-  `fornavn` text NOT NULL,
-  `etternavn` text NOT NULL,
-  `klassekode` char(30) NOT NULL,
-  `bildenr` int(11) DEFAULT NULL,
-  PRIMARY KEY (`brukernavn`),
-  KEY `klassekode` (`klassekode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`brukernavn`, `fornavn`, `etternavn`, `klassekode`, `bildenr`) VALUES
-('lol', 'dsda', 'dskadaskds', 'IS3', 59);
 
 --
 -- Constraints for dumped tables
@@ -166,12 +91,6 @@ ALTER TABLE `answerquestion`
 --
 ALTER TABLE `question`
   ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`correctAnswerID`) REFERENCES `answer` (`answerID`);
-
---
--- Constraints for table `student`
---
-ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`klassekode`) REFERENCES `klasse` (`klassekode`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
