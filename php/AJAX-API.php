740e8cfba4c9d3db7e53d1353926db8ef8ea9023
Fredrik <lol>
2015-03-26 17:55:40 +0100

    wrapped json_encode() around question->toJSON() in AJAX API

--- php/AJAX-API.php
+++ php/AJAX-API.php
@@ -7,7 +7,7 @@ $questions = $db->getQuestions('quiz');
 
 $questionsAsJson = array();
 for($i = 0; $i < count($questions); $i++) {
-    array_push($questionsAsJson, $questions[$i]->toJSON());
+    array_push($questionsAsJson, json_encode($questions[$i]->toJSON()));
 }
 
 $questionsAsJson = json_encode($questionsAsJson);
